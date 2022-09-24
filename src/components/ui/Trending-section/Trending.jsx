5c2542b6aa65cee66c54a6e5157a435d66f66a33
Rasha Saadeh <>
2022-09-23 20:47:03 -0400

    call to opensea collection

--- src/components/ui/Trending-section/Trending.jsx
+++ src/components/ui/Trending-section/Trending.jsx
@@ -6,7 +6,40 @@ import "./trending.css";
 
 import NftCard from "../Nft-card/NftCard";
 
+const axios = require("axios");
+
+
+const getGamingNFTs = async () => {
+  console.log("hello")
+  try {
+    const response = await axios.get(
+      "https://api.opensea.io/api/v1/collection/gods-unchained-collectibles"
+    );
+    // const newToDoList = [...toDoList, {"id": 3, ...}]
+
+    console.log("response  ", response.data.collection);
+    // console.log("response  ", response.data.collection.primary_asset_contracts.slice(0, 4));
+
+    return response.data;
+
+  } catch (error) {
+    console.log(error)
+    return [];
+  }
+};
 const Trending = () => {
+  // componentDidMount() {
+  //   getGamingNFTs()
+  //   // setTimeout(() => {
+  //   // this.setState({
+  //   // isLoading: false,
+  //   // status: "Completed!"
+  //   // });
+  //   // }, 2000);
+  //   }
+  const gamingAssets = getGamingNFTs()
+
+  // object -> data -> collection -> primary_asset_contracts[...]
   return (
     <section>
       <Container>
@@ -14,12 +47,16 @@ const Trending = () => {
           <Col lg="12" className="mb-5">
             <h3 className="trending__title">Trending</h3>
           </Col>
-
           {NFT__DATA.slice(0, 8).map((item) => (
             <Col lg="3" md="4" sm="6" key={item.id} className="mb-4">
               <NftCard item={item} />
             </Col>
           ))}
+          {/* {gamingAssets.slice(0, 8).map((item.data.collection.primary_asset_contracts) => (
+            <Col lg="3" md="4" sm="6" key={item.image_url} className="mb-4">
+              <NftCard item={item} />
+            </Col>
+          ))} */}
         </Row>
       </Container>
     </section>
