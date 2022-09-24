1066e37dfc6563f84d31bf95f256c3e28846952c
Rasha Saadeh <>
2022-09-24 19:16:11 -0400

    unified UI

--- src/components/ui/Nft-card/NftCard.jsx
+++ src/components/ui/Nft-card/NftCard.jsx
@@ -5,10 +5,34 @@ import "./nft-card.css";
 
 import Modal from "../Modal/Modal";
 
-const NftCard = (props) => {
-  const { title, id, currentBid, creatorImg, imgUrl, creator } = props.item;
 
+
+
+
+const NftCard = (props) => {
+ 
+  const { title, id, currentBid, creatorImg, imgUrl, creator } = props.item;
   const [showModal, setShowModal] = useState(false);
+
+  const [message, setMessage] = useState("Purchase")
+
+  async function buyNFT(nft) {
+    setMessage("Sold")
+    /* needs the user to sign the transaction, so will use Web3Provider and sign it */
+    // const web3Modal = new Web3Modal()
+    // const connection = await web3Modal.connect()
+    // const provider = new ethers.providers.Web3Provider(connection)
+    // const signer = provider.getSigner()
+    // const contract = new ethers.Contract(marketplaceAddress, NFTMarketplace.abi, signer)
+  
+    // /* user will be prompted to pay the asking process to complete the transaction */
+    // const price = ethers.utils.parseUnits(nft.currentBid.toString(), 'ether')   
+    // const transaction = await contract.createMarketSale(nft.tokenId, {
+    //   value: price
+    // })
+    // await transaction.wait()
+    // loadNFTs()
+  }
 
   return (
     <div className="single__nft__card">
@@ -22,19 +46,12 @@ const NftCard = (props) => {
         </h5>
 
         <div className="creator__info-wrapper d-flex gap-3">
-          <div className="creator__img">
-            <img src={creatorImg} alt="" className="w-100" />
-          </div>
+         
 
           <div className="creator__info w-100 d-flex align-items-center justify-content-between">
             <div>
-              <h6>Created By</h6>
-              <p>{creator}</p>
-            </div>
-
-            <div>
-              <h6>Current Bid</h6>
-              <p>{currentBid} NEAR</p>
+              <h6>Current Price</h6>
+              <p>{currentBid} ETH</p>
             </div>
           </div>
         </div>
@@ -42,15 +59,16 @@ const NftCard = (props) => {
         <div className=" mt-3 d-flex align-items-center justify-content-between">
           <button
             className="bid__btn d-flex align-items-center gap-1"
-            onClick={() => setShowModal(true)}
+            onClick={() => buyNFT(props.item)}
+            // onClick={() => setShowModal(true)}
           >
-            <i class="ri-shopping-bag-line"></i> Place Bid
+            <i class="ri-shopping-bag-line"></i> {message}
           </button>
 
-          {showModal && <Modal setShowModal={setShowModal} />}
+          {/* {showModal && <Modal setShowModal={setShowModal} />} */}
 
           <span className="history__link">
-            <Link to="#">View History</Link>
+            <Link to="#">View Provenance</Link>
           </span>
         </div>
       </div>
--- src/n2dpuller/nftpuller.js
+++ src/n2dpuller/nftpuller.js
@@ -1,176 +1,128 @@
-import { ethers } from 'ethers';
-import { useEffect, useState } from 'react';
+import { ethers } from "ethers";
+import { useEffect, useState } from "react";
 import React from "react";
-import axios from 'axios';
-import NFTCollection from './NFTCollection.json';
-import { Card, Container, Text, Grid, Button, Image } from '@nextui-org/react';
-import { nftContract, key, displayAmount, mainnet } from './settings';
+import { Row, Col } from "reactstrap";
+import axios from "axios";
+import NFTCollection from "./NFTCollection.json";
+import { Card, Container, Text, Grid, Button, Image } from "@nextui-org/react";
+import { nftContract, key, displayAmount, mainnet } from "./settings";
 import { Network, Alchemy } from "alchemy-sdk";
-
-
-
+import NftCard from "../components/ui/Nft-card/NftCard";
 
 export default function NftPuller() {
-
-
   // Github: https://github.com/alchemyplatform/alchemy-sdk-js
-// Setup: npm install alchemy-sdk
-// Optional Config object, but defaults to demo api-key and eth-mainnet.
-const settings = {
-  apiKey: "bYm06cEEx4W_Evcq4Ed_cCHRfAFGbJO6", // Replace with your Alchemy API Key.
-  network: Network.ETH_MAINNET, // Replace with your network.
-};
+  // Setup: npm install alchemy-sdk
+  // Optional Config object, but defaults to demo api-key and eth-mainnet.
+  const settings = {
+    apiKey: "bYm06cEEx4W_Evcq4Ed_cCHRfAFGbJO6", // Replace with your Alchemy API Key.
+    network: Network.ETH_MAINNET, // Replace with your network.
+  };
 
-const alchemy = new Alchemy(settings);
+  const alchemy = new Alchemy(settings);
 
-// Print the NFT floor price for a contract
+  // Print the NFT floor price for a contract
 
-
-  const [nfts, setNfts] = useState([])
-  const [loadingState, setLoadingState] = useState('not-loaded')
+  const [nfts, setNfts] = useState([]);
+  const [loadingState, setLoadingState] = useState("not-loaded");
   useEffect(() => {
     generateNft();
-    }, [setNfts])
-    
-    async function refreshPage() {
-        window.location.reload();
-    }
-    
-      const generateNft = async () => {
-      const provider = new ethers.providers.JsonRpcProvider(mainnet)
-      const wallet = new ethers.Wallet(key, provider);
-      
-    
-      const nftArray= [ '0xA03e357A09E761E8d486A1419c74bf42e8D1B064','0xd2a3c8f95daaed4ae824dd1f7441ff1f061124d6' ]
-      const itemArray = [];
-      for (let i = 0; i < nftArray.length; i++) 
-      {
+  }, [setNfts]);
 
+  async function refreshPage() {
+    window.location.reload();
+  }
+
+  const generateNft = async () => {
+    const provider = new ethers.providers.JsonRpcProvider(mainnet);
+    const wallet = new ethers.Wallet(key, provider);
+
+    const nftArray = [
+      "0xA03e357A09E761E8d486A1419c74bf42e8D1B064",
+      "0xd2a3c8f95daaed4ae824dd1f7441ff1f061124d6",
+    ];
+    const itemArray = [];
+    for (let i = 0; i < nftArray.length; i++) {
       const contract = new ethers.Contract(nftArray[i], NFTCollection, wallet);
       //FLOOR PRICE CALL HERE
-      
+
       // console.log("FLAG" ,floorPrice)
-      
+
       // .then(console.log);
 
-      const floorPrice = await alchemy.nft.getFloorPrice(nftArray[i]); 
+      const floorPrice = await alchemy.nft.getFloorPrice(nftArray[i]);
 
       console.log("New logger", floorPrice);
 
       // items loop
-      contract.totalSupply().then(result => {
-        let totalSup = parseInt(result, 16)
-  
+      contract.totalSupply().then((result) => {
+        let totalSup = parseInt(result, 16);
+
         var fp = 9;
-      // const floorPrice = async alchemy.nft.getFloorPrice(nftArray[i]).then( (a) => {
-      //                     console.log(a)
-      //                     fp = a.looksRare.floorPrice
-      //                     console.log("Maree log", fp)
-      //                   })
-
-      
-      
-      
-      // ( (a) => {
-      //   console.log(a)
-      //   fp = a.looksRare.floorPrice
-      //   console.log("Maree log", fp)
-      // })
-
-        /*
-        Replace "displayAmount" with "totalSup"
-        below if you want to display all NFTs 
-        in the collection BUT BE CAREFUL, it will render
-        every nft image and possibly freeze your server/browser!!
-        */
         for (let i = 0; i < displayAmount; i++) {
+          var token = i + 1;
+          const owner = contract.ownerOf(token);
+          const rawUri = contract.tokenURI(token);
 
-          var token = i + 1                         
-          const owner = contract.ownerOf(token)
-          const rawUri = contract.tokenURI(token)
-        
-  
-
-          const Uri = Promise.resolve(rawUri)
-          const getUri = Uri.then(value => {
-            let str = value
-            let cleanUri = str.replace('ipfs://', 'https://ipfs.io/ipfs/')
+          const Uri = Promise.resolve(rawUri);
+          const getUri = Uri.then((value) => {
+            let str = value;
+            let cleanUri = str.replace("ipfs://", "https://ipfs.io/ipfs/");
             let metadata = axios.get(cleanUri).catch(function (error) {
               console.log(error.toJSON());
             });
             return metadata;
-          })
+          });
 
-    
-          getUri.then(value => {
-            let rawImg = value.data.image
-            var name = value.data.name
-            var desc = value.data.description
-            var tokenType = value.data.tokenType     
-            let image = rawImg.replace('ipfs://', 'https://ipfs.io/ipfs/')
-            Promise.resolve(owner).then(value => {
+          getUri.then((value) => {
+            let rawImg = value.data.image;
+            var name = value.data.name;
+            var desc = value.data.description;
+            var tokenType = value.data.tokenType;
+            let image = rawImg.replace("ipfs://", "https://ipfs.io/ipfs/");
+            Promise.resolve(owner).then((value) => {
               let ownerW = value;
               let meta = {
-                name: name,
-                img: image,
-                tokenId: token,
+                title: name,
+                id: token,
+                imgUrl: image,
+                currentBid: floorPrice.looksRare.floorPrice,
                 wallet: ownerW,
                 desc,
                 tokenType: tokenType,
-                floorPrice:  floorPrice.looksRare.floorPrice
-              }
-              console.log("meta" , meta)
-              itemArray.push(meta)
-            })
-          })
+              };
+              console.log("meta", meta);
+              itemArray.push(meta);
+            });
+          });
         }
-      })
-
+      });
     }
-      await new Promise(r => setTimeout(r, 5000));
-      setNfts(itemArray)
-      setLoadingState('loaded');
-    }
+    await new Promise((r) => setTimeout(r, 5000));
+    setNfts(itemArray);
+    setLoadingState("loaded");
+  };
 
-if (loadingState === 'loaded' && !nfts.length)
-
+  if (loadingState === "loaded" && !nfts.length)
     return (
-      <div >
-        {
-        nfts.map((nft, i) => {
-          <div>
-          <Card.Image src={nft.img} key={i}/>
-        <h2>No Collections Retrieved</h2>
-        </div>
-})}
-      </div>
-    )
-    return (
-      <Container md>
-        <Text h1 css={{marginLeft:'$10'}} size={'40px'}>NFT Collection</Text>
-        <Button css={{marginLeft: '$10'}} onPress={refreshPage}>Refresh NFTs</Button>
-      <Grid.Container gap={3}>
+      <div>
         {nfts.map((nft, i) => {
-            return (
-              <Grid >
-                <a>
-                  <Card isHoverable key={i} css={{ mw: "270px", marginRight: '$1', boxShadow:'0px 2px 12px #000000' }} variant="bordered">
-                    <Card.Image src={nft.img} />
-                    <Card.Body md css={{background:"$gradient"}}>
-                    <Text css={{color:'$white'}} h2>{nft.name}</Text>
-                    <Text h3 css={{color:'$white'}}>NFT ID: {nft.tokenId}</Text>
-                    <Text css={{color:'$white'}}>{nft.desc}</Text>
-                    <Text h3 css={{color:'$white'}}> Price:{nft.floorPrice}</Text>
-
-                    
-
-                    </Card.Body>
-                  </Card>
-                </a>
-              </Grid>
-            )
-          })}
-      </Grid.Container>
-    </Container>
-    )
+          <div>
+            <Card.Image src={nft.img} key={i} />
+            <h2>No Collections Retrieved</h2>
+          </div>;
+        })}
+      </div>
+    );
+  return (
+    <div>
+       <Row>
+       {nfts.map((nft) => (
+        <Col lg="3" md="4" sm="6" className="mb-4">
+          <NftCard key={nft.id} item={nft} />
+        </Col>
+      ))}
+       </Row>
+      
+    </div>
+  );
 }
