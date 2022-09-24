7a78eea2a7590b796b814663a992ccb80154a9c0
mireeantar <mireeantar@gmail.com>
2022-09-24 02:09:46 -0400

    add: nfts

--- src/components/ui/Trending-section/Trending.jsx
+++ src/components/ui/Trending-section/Trending.jsx
@@ -5,8 +5,45 @@ import { NFT__DATA } from "../../../assets/data/data";
 import "./trending.css";
 
 import NftCard from "../Nft-card/NftCard";
+import Nftpuller from '../../../n2dpuller/nftpuller'
 
+const axios = require("axios");
+
+
+// const getGamingNFTs = async () => {
+//   console.log("hello")
+//   try {
+//     const response = await axios.get(
+//       "https://api.opensea.io/api/v1/collection/gods-unchained-collectibles"
+//     );
+//     // const newToDoList = [...toDoList, {"id": 3, ...}]
+
+//     console.log("response  ", response.data.collection);
+//     // console.log("response  ", response.data.collection.primary_asset_contracts.slice(0, 4));
+
+//     return response.data;
+
+//   } catch (error) {
+//     console.log(error)
+//     return [];
+//   }
+// };
 const Trending = () => {
+  // componentDidMount() {
+  //   getGamingNFTs()
+  //   // setTimeout(() => {
+  //   // this.setState({
+  //   // isLoading: false,
+  //   // status: "Completed!"
+  //   // });
+  //   // }, 2000);
+  //   }
+  // const gamingAssets = getGamingNFTs()
+  
+
+  //0xF5D669627376EBd411E34b98F19C868c8ABA5ADA
+
+  // object -> data -> collection -> primary_asset_contracts[...]
   return (
     <section>
       <Container>
@@ -15,11 +52,20 @@ const Trending = () => {
             <h3 className="trending__title">Trending</h3>
           </Col>
 
-          {NFT__DATA.slice(0, 8).map((item) => (
+          <Nftpuller/>
+
+
+
+          {/* {NFT__DATA.slice(0, 8).map((item) => (
             <Col lg="3" md="4" sm="6" key={item.id} className="mb-4">
               <NftCard item={item} />
             </Col>
-          ))}
+          ))} */}
+          {/* {gamingAssets.slice(0, 8).map((item.data.collection.primary_asset_contracts) => (
+            <Col lg="3" md="4" sm="6" key={item.image_url} className="mb-4">
+              <NftCard item={item} />
+            </Col>
+          ))} */}
         </Row>
       </Container>
     </section>
